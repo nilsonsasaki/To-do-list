9a57770058a3405359b6399df6272fb36b41d97f
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-16 08:25:18 -0300

    added date picker

--- app/src/main/java/com/nilsonsasaki/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/nilsonsasaki/todolist/ui/AddTaskActivity.kt
@@ -2,7 +2,11 @@ package com.nilsonsasaki.todolist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.nilsonsasaki.todolist.databinding.ActivityAddTaskBinding
+import com.nilsonsasaki.todolist.extensions.format
+import com.nilsonsasaki.todolist.extensions.text
+import java.util.*
 
 class AddTaskActivity : AppCompatActivity() {
 
@@ -13,5 +17,19 @@ class AddTaskActivity : AppCompatActivity() {
 
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.dateTextField.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.dateTextField.text = Date(it+ offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
     }
 }
